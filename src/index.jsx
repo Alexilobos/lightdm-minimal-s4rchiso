86082da066589c65f12998d62743e4ab52f7a82d
Sergio Alejandro Ribera Costa <sergioalejandroriberacosta@gmail.com>
2021-04-19 04:21:23 -0400

    Init github Page for Demo

--- src/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
-import Home from './screens/Home';
-
-ReactDOM.render(
-    <Home/>,
-    document.getElementById('root')
-);
